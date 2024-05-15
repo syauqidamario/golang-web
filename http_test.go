85ebc4ca737c44de5b9dc59832b103856bbe3411
syauqidamario <syauqi.djohan@student.umn.ac.id>
2024-05-15 15:50:27 +0700

    Handler, HTTP, and Server

---
+++ http_test.go
@@ -0,0 +1,26 @@
+package golang_web
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(writer http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(writer, "Hello World")
+}
+
+func TestHelloHandler(t *testing.T){
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	bodyString := string(body)
+
+	fmt.Println(bodyString)
+}
